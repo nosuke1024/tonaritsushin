177db36a83512d20aaad5f80d86a9fd1e36608f4
nosuke1024 <kim11yuno@gmail.com>
2024-09-10 21:35:41 +0900

    _prefixの追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,8 +5,8 @@ class Post < ApplicationRecord
   belongs_to :user
 
   # ENUMの定義
-  enum previous_carrier: { 大手キャリア: 0, 大手キャリアオンラインプラン: 1, 大手キャリアサブブランド: 2, 楽天モバイルまたは格安SIM: 3 }
-  enum new_carrier: { 大手キャリア: 0, 大手キャリアオンラインプラン: 1, 大手キャリアサブブランド: 2, 楽天モバイルまたは格安SIM: 3 }
+  enum previous_carrier: { 大手キャリア: 0, 大手キャリアオンラインプラン: 1, 大手キャリアサブブランド: 2, 楽天モバイルまたは格安SIM: 3 }, _prefix: :previous_carrier
+  enum new_carrier: { 大手キャリア: 0, 大手キャリアオンラインプラン: 1, 大手キャリアサブブランド: 2, 楽天モバイルまたは格安SIM: 3 }, _prefix: :new_carrier
   enum price_difference: { '1001円以上ダウン': 0, '1001円以上アップ': 1, '1000円ダウン～1000円アップ': 2 }
   enum device_payment: { 'あり': 0, 'なし': 1 }
   enum purchase_method: { 乗り換え: 0, オンラインショップ: 1, 店頭による機種変更: 2 }
