09d498abbcfec2dfe7fcf44e4fed573ac5043354
nosuke1024 <kim11yuno@gmail.com>
2024-09-13 08:25:39 +0900

    enumの追加

--- Gemfile
+++ Gemfile
@@ -45,6 +45,9 @@ gem 'rails-i18n'
 # 13ブランチで導入
 gem 'byebug', '~> 11.1'
 
+# 18ブランチで導入
+gem 'enum_help'
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ], require: "debug/prelude"
--- app/models/post.rb
+++ app/models/post.rb
@@ -5,9 +5,9 @@ class Post < ApplicationRecord
   belongs_to :user
 
   # ENUMの定義
-  enum previous_carrier: { 大手キャリア: 0, 大手キャリアオンラインプラン: 1, 大手キャリアサブブランド: 2, 楽天モバイルまたは格安SIM: 3 }, _prefix: :previous_carrier
-  enum new_carrier: { 大手キャリア: 0, 大手キャリアオンラインプラン: 1, 大手キャリアサブブランド: 2, 楽天モバイルまたは格安SIM: 3 }, _prefix: :new_carrier
-  enum price_difference: { '1001円以上ダウン': 0, '1001円以上アップ': 1, '1000円ダウン～1000円アップ': 2 }
-  enum device_payment: { 'あり': 0, 'なし': 1 }
-  enum purchase_method: { 乗り換え: 0, オンラインショップ: 1, 店頭による機種変更: 2 }
+  enum previous_carrier: { major_carrier: 0, online_plan: 1, sub_brand: 2, mvno: 3 }, _prefix: :previous_carrier
+  enum new_carrier: { major_carrier: 0, online_plan: 1, sub_brand: 2, mvno: 3 }, _prefix: :new_carrier
+  enum price_difference: { cheaper: 0, more_expensive: 1, no_change: 2 }
+  enum device_payment: { yes: 0, no: 1 }
+  enum purchase_method: { carrier_switch: 0, online_shop: 1, shop: 2 }
 end
