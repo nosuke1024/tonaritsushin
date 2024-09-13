af15e49e164360ac9f1ac507d67e7a7f5bc07112
nosuke1024 <kim11yuno@gmail.com>
2024-09-13 20:05:16 +0900

    更新と削除の追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,9 +25,26 @@ class PostsController < ApplicationController
     @post = current_user.posts.find(params[:id])
   end
 
+  def update
+    @post = current_user.posts.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_path(@post), success: t('defaults.flash_message.updated')
+    else
+      flash.now
+      flash.now[:danger] = t('defaults.flash_message.not_updated')
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    post = current_user.posts.find(params[:id])
+    post.destroy!
+    redirect_to posts_path, success: t('defaults.flash_message.deleted'), status: :see_other
+  end
+
   private
 
   def post_params
-    params.require(:post).permit(:title, :body)
+    params.require(:post).permit(:title, :body, :previous_carrier, :new_carrier, :price_difference, :device_payment, :purchase_method)
   end
 end
