941b667edc6e30778114f5227f331b00d555e835
nosuke1024 <kim11yuno@gmail.com>
2024-09-14 10:11:33 +0900

    日本の時間とユーザー名の取得

--- config/application.rb
+++ config/application.rb
@@ -15,15 +15,9 @@ module Myapp
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
-
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
-
+    # 追加した通常の言語
     config.i18n.default_locale = :ja
+    # タイムゾーンの設定
+    config.time_zone = 'Tokyo'
   end
 end
