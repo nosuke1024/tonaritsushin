4b7dc64a85cc73cd04dd6e200cf5d96457033342
nosuke1024 <kim11yuno@gmail.com>
2024-10-03 14:14:24 +0900

    お気に入りテーブルの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_18_002046) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_02_062503) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "favorites", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_favorites_on_post_id"
+    t.index ["user_id", "post_id"], name: "index_favorites_on_user_id_and_post_id", unique: true
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.integer "title", default: 0, null: false
@@ -40,5 +50,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_18_002046) do
     t.index ["name"], name: "index_users_on_name", unique: true
   end
 
+  add_foreign_key "favorites", "posts"
+  add_foreign_key "favorites", "users"
   add_foreign_key "posts", "users"
 end
