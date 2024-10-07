804c3ad19aebf76f0a872173ac5b2a0cf0be2403
nosuke1024 <kim11yuno@gmail.com>
2024-10-07 14:08:55 +0900

    GEMの導入

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,17 +3,15 @@ class FavoritesController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
-    current_user.favorite(@post)
-
-    respond_to do |format|
-      format.turbo_stream do
-        render turbo_stream: turbo_stream.replace("favorite_#{@post.id}", partial: 'posts/favorite', locals: { post: @post })
-      end
-    end
+    favorite = @post.favorites.new(user_id: current_user.id)
+    favorite.save
+    # redirect_back(fallback_location: root_path)
   end
 
   def destroy
-    @post = current_user.post.find(params[:id]).post
-    current_user.favorite(@post)
+    @post = Post.find(params[:post_id])
+    favorite = current_user.favorites.find_by(post_id: @post.id)
+    favorite.destroy
+    # redirect_back(fallback_location: root_path)
   end
 end
