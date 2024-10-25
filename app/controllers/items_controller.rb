7c34aa4303a90b57837e7f2e21ae90ceaa632efe
nosuke1024 <kim11yuno@gmail.com>
2024-10-25 15:38:48 +0900

    新品と中古のラベルの追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,8 +4,27 @@ class ItemsController < ApplicationController
   end
 
   def search
-    if params[:keyword]
-      @items = RakutenWebService::Ichiba::Item.search(keyword: params[:keyword])
+    @search_params = item_search_params
+    search_conditions = {}
+
+    # キーワードに商品状態を追加
+    keyword = @search_params[:keyword].to_s
+    keyword += " " + @search_params[:condition].to_s if @search_params[:condition].present?
+    search_conditions[:keyword] = @search_params[:keyword].presence || "スマホ" # デフォルトキーワードを設定
+  
+    # 楽天モバイルで購入できる商品に絞り込み
+    if @search_params[:rakuten] == "1"
+      search_conditions[:carrier] = 1 # 楽天モバイルのキャリアコードを指定
     end
+  
+    if search_conditions.present?
+      @items = RakutenWebService::Ichiba::Item.search(search_conditions)
+    end
+  end
+
+  private
+
+  def item_search_params
+    params.permit(:condition, :rakuten, :keyword)
   end
 end
