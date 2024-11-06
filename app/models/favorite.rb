aeaee31c828220887c0131b526ad7750b60a1c31
nosuke1024 <kim11yuno@gmail.com>
2024-11-06 17:26:46 +0900

    通知に対するモデル間の定義

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -2,5 +2,8 @@ class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
+  # ポリモーフィック関連付け
+  has_many :notifications, as: :notifiable, dependent: :destroy
+
   validates :user_id, uniqueness: { scope: :post_id }
 end
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,2 +1,10 @@
 class Notification < ApplicationRecord
+  belongs_to :user
+  # 通知の訪問者（アクションを起こしたユーザー）
+  belongs_to :visitor, class_name: 'User', foreign_key: 'visitor_id'
+  # 通知の受信者（アクションの対象となったユーザー）
+  belongs_to :visited, class_name: 'User', foreign_key: 'visited_id'
+
+  # ポリモーフィック関連付け
+  belongs_to :notifiable, polymorphic: true
 end
