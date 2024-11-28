115ec53d8d8b07254315045857be9f3ab89bba91
nosuke1024 <kim11yuno@gmail.com>
2024-11-28 14:14:00 +0900

    静的OGPの作成

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,28 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: 'Smart Phone Choice',
+      title: 'スマホの購入における情報収集から購入まで網羅的にサポートするサービス',
+      reverse: true,
+      charset: 'utf-8',
+      description: 'もう騙されない。損しない。賢いスマホの買い方・料金プランの選び方をサポート。みんなの声を参考に、あなただけの最適なプランを見つけよう',
+      keywords: 'スマホ,通信キャリア,格安SIM,サブブランド,乗り換え,SIMのみ,共有,みんなで,賢く',
+      canonical: 'https://smart-phone-choice.com',
+      separator: '|',
+      og:{
+        site_name: :site,
+        title: :title,
+        description: :description,
+        type: 'website',
+        url: 'https://smart-phone-choice.com',
+        image: image_url('app_logo.png'),
+        local: 'ja-JP'
+      },
+      twitter: {
+        card: 'summary',
+        site: '@emieminosuke',
+        image: image_url('app_logo.png')
+      }
+    }
+  end
 end
