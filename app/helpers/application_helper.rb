844115879c40082ed1d46b3dc2829cd67794baa5
nosuke1024 <kim11yuno@gmail.com>
2024-11-28 14:51:24 +0900

    Twitterの#タグ追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,26 +2,28 @@ module ApplicationHelper
   def default_meta_tags
     {
       site: 'Smart Phone Choice',
-      title: 'スマホの購入における情報収集から購入まで網羅的にサポートするサービス',
+      title: 'スマホの購入における情報収集から購入までサポート',
       reverse: true,
       charset: 'utf-8',
       description: 'もう騙されない。損しない。賢いスマホの買い方・料金プランの選び方をサポート。みんなの声を参考に、あなただけの最適なプランを見つけよう',
       keywords: 'スマホ,通信キャリア,格安SIM,サブブランド,乗り換え,SIMのみ,共有,みんなで,賢く',
-      canonical: 'https://smart-phone-choice.com',
+      canonical: 'https://smart-phone-choice.com/',
       separator: '|',
       og:{
         site_name: :site,
         title: :title,
         description: :description,
         type: 'website',
-        url: 'https://smart-phone-choice.com',
+        url: 'https://smart-phone-choice.com/',
         image: image_url('app_logo.png'),
         local: 'ja-JP'
       },
       twitter: {
         card: 'summary',
         site: '@emieminosuke',
-        image: image_url('app_logo.png')
+        image: image_url('app_logo.png'),
+        title: 'Smart Phone Choiceでスマホ選び #すまちょい',
+        description: 'もう騙されない。損しない。賢いスマホの買い方・料金プランの選び方をサポート。'
       }
     }
   end
