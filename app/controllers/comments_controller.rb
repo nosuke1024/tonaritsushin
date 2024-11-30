32aa1304f2b5b48c517e6d8a07774d4b09954658
nosuke1024 <kim11yuno@gmail.com>
2024-11-30 21:36:32 +0900

    コメントの削除機能の追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,6 +8,12 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    comment = current_user.comments.find(params[:id])
+    comment.destroy!
+    redirect_to post_path(comment.post), flash: { danger: t('defaults.flash_message.deleted') }, status: :see_other
+  end
+
   private
 
   def comment_params
