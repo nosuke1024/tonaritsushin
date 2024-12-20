dd21f135c754b2c3c00d18f10ab6f9c848fb6b98
nosuke1024 <kim11yuno@gmail.com>
2024-12-20 13:02:03 +0900

    plan_diagnoses_controller_testの修正

--- test/controllers/plan_diagnoses_controller_test.rb
+++ test/controllers/plan_diagnoses_controller_test.rb
@@ -1,13 +1,12 @@
 require "test_helper"
 
 class PlanDiagnosesControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get plan_diagnoses_new_url
-    assert_response :success
-  end
-
-  test "should get create" do
-    get plan_diagnoses_create_url
-    assert_response :success
+  test "should post create" do
+    post plan_diagnoses_path, params: { 
+      question1: "value1",
+      question2: "value2",
+      question3: "value3"
+    }
+    assert_redirected_to result_plan_diagnoses_path
   end
 end
