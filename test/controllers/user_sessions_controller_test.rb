46d21ee98fe6e94d5fc0afb8a0d97c7e86bbaf80
nosuke1024 <kim11yuno@gmail.com>
2024-12-20 15:34:25 +0900

    テストユーザーを追加して、ちゃんとアクションを定義

--- test/controllers/user_sessions_controller_test.rb
+++ test/controllers/user_sessions_controller_test.rb
@@ -1,19 +1,28 @@
 require "test_helper"
 
 class UserSessionsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = users(:one)  # fixtureのoneを参照
+  end
+
   test "should get new" do
     get login_path   # user_sessions_new_url から変更
     assert_response :success
   end
 
   test "should get create" do
-    post login_path, params: { email: 'test@example.com', password: 'password' }   # HTTPメソッドとパスを変更
+    post login_path, params: {
+      email: @user.email,
+      password: 'password'  # fixtureで設定したパスワード
+    }
     assert_response :redirect
     assert_redirected_to posts_path
   end
 
   test "should get destroy" do
-    delete logout_path    # user_sessions_destroy_url から変更
+    # ログインしたあとに削除
+    post login_path, params: { email: @user.email, password: 'password' }
+    delete logout_path
     assert_response :see_other
     assert_redirected_to root_path
   end
