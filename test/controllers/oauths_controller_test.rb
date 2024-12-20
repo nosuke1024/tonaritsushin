94877b2c91655a673e24554629366874c9c431f1
nosuke1024 <kim11yuno@gmail.com>
2024-12-20 15:51:04 +0900

    テストコードに環境変数の追加

--- test/controllers/oauths_controller_test.rb
+++ test/controllers/oauths_controller_test.rb
@@ -1,6 +1,12 @@
 require "test_helper"
 
 class OauthsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    # テスト用の環境変数を設定
+    ENV['LINE_Sorcery_ID'] = 'test_client_id'
+    ENV['LINE_Sorcery_SECRET'] = 'test_client_secret'
+  end
+
   test "should get callback" do
     # 必要最小限のパラメータのみ送信
     get oauth_callback_path, params: {
