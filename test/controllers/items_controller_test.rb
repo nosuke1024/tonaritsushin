619fc81e2aa6d291a42d07c0f83048f23b30b6e3
nosuke1024 <kim11yuno@gmail.com>
2024-12-20 16:07:21 +0900

    外部APIに関するテストコードの追加

--- test/controllers/items_controller_test.rb
+++ test/controllers/items_controller_test.rb
@@ -7,8 +7,14 @@ class ItemsControllerTest < ActionDispatch::IntegrationTest
     post login_path, params: { email: @user.email, password: 'password' }
   end
 
-  test "should get index" do
-    get items_index_url
+  test "should get index for logged in user" do
+    get items_path
     assert_response :success
+  end
+
+  test "should redirect to login when not logged in" do
+    delete logout_path  # ログアウトする
+    get items_path
+    assert_redirected_to login_path  # ログインページにリダイレクトされることを確認
   end
 end
--- test/controllers/oauths_controller_test.rb
+++ test/controllers/oauths_controller_test.rb
@@ -1,19 +1,8 @@
 require "test_helper"
 
 class OauthsControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    # テスト用の環境変数を設定
-    ENV['LINE_Sorcery_ID'] = 'test_client_id'
-    ENV['LINE_Sorcery_SECRET'] = 'test_client_secret'
-  end
-
-  test "should get callback" do
-    # 必要最小限のパラメータのみ送信
-    get oauth_callback_path, params: {
-      provider: 'line',
-      code: 'dummy_auth_code'
-    }
-    assert_response :redirect # リダイレクトされることを確認
-    assert_redirected_to root_path # リダイレクト先の確認
+  test "should redirect to login path" do
+    get oauth_path
+    assert_response :redirect
   end
 end
