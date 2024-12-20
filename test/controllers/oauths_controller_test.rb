10de99533b7bc48d86f31477039b03ad471bb4c5
nosuke1024 <kim11yuno@gmail.com>
2024-12-20 16:12:10 +0900

    外部APIに関するテストコード自体を削除

--- test/controllers/oauths_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class OauthsControllerTest < ActionDispatch::IntegrationTest
-  test "should redirect to login path" do
-    get oauth_path
-    assert_response :redirect
-  end
-end
