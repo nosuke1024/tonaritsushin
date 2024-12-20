238960e46661387860b920a6870d25c9114f2330
nosuke1024 <kim11yuno@gmail.com>
2024-12-20 21:38:31 +0900

    バリテーションの記載方法の追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   validates :password, length: { minimum: 8 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
-  validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)/, message: "はアルファベットと数字を含める必要があります。" }, if: -> { new_record? || changes[:crypted_password] }
+  validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d).*\z/, message: "はアルファベットと数字を含める必要があります。" }, if: -> { new_record? || changes[:crypted_password] }
   validates :name, presence: true, length: { maximum: 255 }
   validates :email, presence: true, uniqueness: true
 
