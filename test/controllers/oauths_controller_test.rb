647fdddae65c8fbc4df953b0c1285093efd1b67b
nosuke1024 <kim11yuno@gmail.com>
2024-12-20 20:17:47 +0900

    githubアクションのテストの成功

--- test/controllers/oauths_controller_test.rb
+++ test/controllers/oauths_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class OauthsControllerTest < ActionDispatch::IntegrationTest
   test "should redirect to login path" do
-    get auth_at_provider_path(provider: 'line')
+    get auth_at_provider_path(provider: "line")
     assert_response :redirect
   end
 end
