dbcc3b925a14ea19be73db090c69cf1b405b6fb1
nosuke1024 <kim11yuno@gmail.com>
2024-12-20 20:06:31 +0900

    再度修正

--- test/controllers/items_controller_test.rb
+++ test/controllers/items_controller_test.rb
@@ -1,29 +1,8 @@
 require "test_helper"
 
 class ItemsControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    @user = users(:one)
-    login_as(@user)
-  end
-
-  test "should get index for logged in user" do
-    get items_path
-    assert_response :success
-  end
-
   test "should redirect to login when not logged in" do
-    logout
     get items_path
     assert_redirected_to login_path
-  end
-
-  private
-
-  def login_as(user)
-    post login_url, params: { email: user.email, password: "password" }
-  end
-
-  def logout
-    delete logout_url
   end
 end
