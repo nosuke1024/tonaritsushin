14881f11af257280eed6b8a7d1a9681dc6ba8b61
nosuke1024 <kim11yuno@gmail.com>
2024-12-20 20:12:47 +0900

    再度修正

--- test/controllers/oauths_controller_test.rb
+++ test/controllers/oauths_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class OauthsControllerTest < ActionDispatch::IntegrationTest
   test "should redirect to login path" do
-    get oauth_path
+    get auth_at_provider_path(provider: 'line')
     assert_response :redirect
   end
 end
