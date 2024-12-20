729fd563ae5cdff0fdc9b21839d5c600b6915139
nosuke1024 <kim11yuno@gmail.com>
2024-12-20 14:48:50 +0900

    test/controllers/oauths_controller_test.rbの修正

--- test/controllers/oauths_controller_test.rb
+++ test/controllers/oauths_controller_test.rb
@@ -2,15 +2,15 @@ require "test_helper"
 
 class OauthsControllerTest < ActionDispatch::IntegrationTest
   test "should get oauth" do
-    get oauth_path(provider: 'line')  # providerパラメータを追加
+    # :auth_at_provider パスを使用
+    get auth_at_provider_path(provider: 'line')
     assert_response :success
   end
 
   test "should get callback" do
-    get oauth_callback_path(provider: 'line')  # providerパラメータを追加
-    # コールバックは通常リダイレクトするので
+    # 直接パスを指定
+    get oauth_callback_path
     assert_response :redirect
-    # ログイン失敗時はroot_pathにリダイレクト
     assert_redirected_to root_path
   end
 end
